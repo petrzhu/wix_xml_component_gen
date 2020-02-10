33581076f60ec1552169120a8ce976acf2bea21c
petrzhu <petr@fusetwin.com>
2020-02-10 12:57:39 +0200

    added README & script file

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # wix_xml_component_gen
 Python script for generating component &amp; reference elements for WiX Toolset.
+
+# Purpose
+This script's purpose is to generate the list of components combined in the components group and to generate components' references.
+
+The generated content is shown in the console output.
+
+# Usage
+```python wix_xml_component_gen.py -d {DIRECTORY_WITH_FILES} -e {YOUR_EXTENSIONS_NAME}```
+
+It is possible to pass several values as arguments, example:
+
+```python wix_xml_component_gen.py -d C:\dir1 C:\dir2 -e exe manifest config```
+
+Modify **extensionIdDict** dictionary to add or change IDs of ComponentGroup dedicated for different type of files.
---
+++ wix_component_xml_generator.py
@@ -0,0 +1,134 @@
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import Element, SubElement, dump
+from os.path import isfile, join, splitext
+from os import listdir
+import sys
+
+extensionIdDict={".dll":"DLLComponents", ".exe":"ProductComponents", 
+				".xml":"AdditionalComponents", ".manifest":"AdditionalComponents",
+				".config":"AdditionalComponents", ".application":"AdditionalComponents"}
+
+def indent(elem, level=0):
+  i = "\n" + level*"  "
+  if len(elem):
+    if not elem.text or not elem.text.strip():
+      elem.text = i + "  "
+    if not elem.tail or not elem.tail.strip():
+      elem.tail = i
+    for elem in elem:
+      indent(elem, level+1)
+    if not elem.tail or not elem.tail.strip():
+      elem.tail = i
+  else:
+    if level and (not elem.tail or not elem.tail.strip()):
+      elem.tail = i
+
+def generateComponentGroup(dirPath,_id,extension):
+	componentGroupAtt = {"Id":_id,"Directory":"INSTALLFOLDER","Source":dirPath}
+	componentGroup = Element("ComponentGroup",attrib=componentGroupAtt)
+	fileList = [f for f in listdir(dirPath) if (isfile(join(dirPath,f)) and (splitext(f)[1])==extension)]
+	
+	if (fileList != []):
+		for idx,f in enumerate(fileList):
+			fName = splitext(f)[0]
+			fFormat = splitext(f)[1]
+			componentAttr = {"Id":fFormat.upper()[1:]+"_"+fName}
+			component = SubElement(componentGroup, "Component", attrib=componentAttr)
+			fileComponentAttr = {"Name":f}
+			if (extension == ".exe"):
+				fileComponentAttr.update({"KeyPath":"yes"})
+			fileComponent = SubElement(component, "File",attrib=fileComponentAttr)
+		return componentGroup
+	
+	return None
+
+def generateFragment(componentGroup):
+	fragment = Element("Fragment")
+	fragment.append(componentElement)
+	return fragment
+
+def generateFragment(componentGroupList):
+	fragment = Element("Fragment")
+	for componentGroup in componentGroupList:
+		if (list(fragment) != []):
+			idx = next((i for i,v in enumerate(fragment) if (componentGroup.tag == v.tag) and (componentGroup.attrib == v.attrib)),-1)
+			if (idx != -1):
+				for component in componentGroup:
+					list(fragment)[idx].append(component)
+			else:
+				fragment.append(componentGroup)
+		else:
+			fragment.append(componentGroup)
+	return fragment
+
+def generateFeature(componentGroup,id,title,level="1"):
+	featureAttr = {"Id":id,"Title":title,"Level":level}
+	featureComponent = Element("Feature",featureAttr)
+	componentGroupRefAttr = {}
+	for i in componentGroup.items():
+		if i[0]=="Id":
+			componentGroupRefAttr.update({i[0]:i[1]})
+	componentGroupRef = SubElement(featureComponent,"ComponentGroupRef",attrib=componentGroupRef)
+	return componentGroupRef
+
+def generateFeature(componentGroupList,id,title,level="1"):
+	featureAttr = {"Id":id,"Title":title,"Level":level}
+	featureComponent = Element("Feature",featureAttr)
+	cGroupRefAttrList = []
+	for componentGroup in componentGroupList:
+		for i in componentGroup.items():
+			componentGroupRefAttr = {}
+			if i[0]=="Id":
+				componentGroupRefAttr.update({i[0]:i[1]})
+				if (componentGroupRefAttr in cGroupRefAttrList):
+					break
+				else:
+					cGroupRefAttrList.append(componentGroupRefAttr)
+			else:
+				break
+			componentGroupRef = SubElement(featureComponent,"ComponentGroupRef",attrib=componentGroupRefAttr)
+			componentGroupRef.tail = "\n"
+	
+	return featureComponent
+
+
+
+def main(**kwargs):
+	
+
+	ext = []
+	directory = []
+	componentGroupList = []
+
+	for key, val in kwargs.items():
+		if (key=="extension"):
+			ext = val
+		elif (key=="dir"):
+			directory=val
+	try:		
+		for xt in ext:
+			xt = "."+xt
+			_id = extensionIdDict[xt]
+			for d in directory:
+				c = generateComponentGroup(d,_id,xt)
+				if (c != None):
+					componentGroupList.append(c)
+		
+		fragment = generateFragment(componentGroupList)
+		indent(fragment)
+		print(ET.tostring(fragment).decode("utf-8"))
+
+		feature = generateFeature(componentGroupList,"ProductFeature", title="LasermarkingTracker")
+		indent(feature)
+		print(ET.tostring(feature).decode("utf-8"))
+	
+	except Exception as e:
+		print(e)
+
+if __name__ == "__main__":
+	import argparse
+	parser = argparse.ArgumentParser(description="Python script for generating some of the parts of the WiX xml config file.")
+	parser.add_argument("-d", "--dir", nargs='+', required=True, help="set the directory of the target files")
+	parser.add_argument("-e", "--ext", nargs='+', required=True, help="file extensions to add to the configuration")
+	args = parser.parse_args()
+	main(dir=args.dir, extension=args.ext)
